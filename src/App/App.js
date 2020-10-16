8378ff14a88366dc935ff6db1e1c3f3ae6e349d5
Grzegorz Sikora <greesik@gmail.com>
2020-10-16 20:39:02 +0200

    first commit; sections arrangement

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import MainScreen from "../MainScreen/MainScreen";
+import Header from "../Header/Header";
+import About from "../About/About";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <MainScreen/>
+      <About/>
+      <Contact/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainScreen/MainScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./MainScreen.css";
+
+const MainScreen = () => {
+    return (
+        <section className={"main-screen"}>
+            MainScreen
+        </section>
+    );
+};
+
+export default MainScreen;
